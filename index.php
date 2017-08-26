66f9a0afc01a113bed36edd53c209eb77ddb6168
nathanramorim <nathan.ramorim@gmail.com>
2017-08-26 01:22:31 -0300

    consegui separar os names e categorias

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
-
-
 echo '<pre>';
 $arr = load_file('movies');
-var_dump($arr);
-echo '</pre>';    
+$i = 0;
+$movies[$i]['name'] = '';
+$movies[$i]['category'] = '';
+foreach ($arr as $value) {
+    
+    $movies[$i]['name'] = $value[1];
+    $movies[$i]['category'] = $value[2];
+    if(isset($value[3])){
+        $movies[$i]['category'] .= '|'.$value[3];
+    }
+    $i++;
+    
+}
+var_dump($movies);
 
+echo '</pre>';    
 
 
 function load_file ($file_name){
@@ -15,6 +26,7 @@ function load_file ($file_name){
     while (!feof($arr)){
         $movies[$i] = explode('::',fgets($arr));
         $i++;
+        
     }
     return $movies;
 }
