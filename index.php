cd07cae7eb88b420c31de25da99f1e066d7acb9f
nathanramorim <nathan.ramorim@gmail.com>
2017-08-26 00:55:52 -0300

    feito somente a leitura de conteudo

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
-$arr = file('movies.dat');
-foreach ($arr as $value) {
-    $movie[] = explode('::',$value);
-}
-
 
 echo '<pre>';
-var_dump($movie);
-echo '</pre>';
+$arr = load_file('movies');
+var_dump($arr);
+echo '</pre>';    
+
+
+
+function load_file ($file_name){
+    $file = $file_name.'.dat';
+    $arr = fopen($file,'r');
+    $i = 1;
+    while (!feof($arr)){
+        $movies[$i] = explode('::',fgets($arr));
+        $i++;
+    }
+    return $movies;
+}
